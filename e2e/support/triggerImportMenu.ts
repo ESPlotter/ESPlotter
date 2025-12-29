07b62639e2afcfcd8909d3ff084208ec3e7cdf62
nabby27 <26057762+nabby27@users.noreply.github.com>
2025-12-29 14:32:04 +0100

    refactor: simplify triggerImportMenu function definition

--- e2e/support/triggerImportMenu.ts
+++ e2e/support/triggerImportMenu.ts
@@ -2,10 +2,7 @@ import { ElectronApplication, Page } from '@playwright/test';
 
 import { clickMenuItem } from './clickMenuItem';
 
-export async function triggerImportMenu(
-  app: ElectronApplication,
-  mainPage: Page,
-): Promise<void> {
+export async function triggerImportMenu(app: ElectronApplication, mainPage: Page): Promise<void> {
   await mainPage.bringToFront();
   await clickMenuItem(app, ['File', 'Import']);
 }
